d867f1b83aa6b16a90c79bea74d05bed4f004426
Kevin Wood <kevin.wood@my.utsa.edu>
2020-04-25 14:34:03 -0500

    Added test mode to test performance of fitness function, updated code to use new fitness function

--- Code/ga/solve_itc_baseline_ga.py
+++ Code/ga/solve_itc_baseline_ga.py
@@ -32,7 +32,7 @@ from csp_utils import display_solution, display_solution_in_table
 from read_itc_data_file import read_itc_data_file
 from timetabling_csp import TimetablingCSP
 from timeslot_csp import TimeSlot
-from verify_solution import verify_solution, score_solution
+from verify_solution import verify_solution, score_solution, fitness_function
 
 
 # USE_ONE_DAY_CLASSES = True
@@ -231,18 +231,62 @@ def set_up_csp(file_name, verbose=False):
 
     return variables, domains, constraints, curricula, time_slots
 
+import timeit
+#Fitness performance testing
+def perf_test(file_name):    
+    SETUP_CODE ='''
+file_name = "'''+file_name+'''"
+from itc_ga_framework import init_population
+from solve_itc_baseline_ga import set_up_csp
+from verify_solution import score_solution
+variables, domains, constraints, curricula, time_slots = set_up_csp(file_name)
+population = init_population(100, variables, domains)'''
+    TEST_CODE = '''
+list(map(lambda x: score_solution(file_name, x), population))'''
+    print(timeit.timeit(TEST_CODE,SETUP_CODE, number = 10))
+    
+    SETUP_CODE2 ='''
+file_name = "'''+file_name+'''"
+from itc_ga_framework import init_population
+from solve_itc_baseline_ga import set_up_csp
+from read_itc_data_file import read_itc_data_file
+from verify_solution import fitness_function
+variables, domains, constraints, curricula, time_slots = set_up_csp(file_name)
+courses, rooms, num_days, periods_per_day, unavail_constraints, curricula = read_itc_data_file(file_name)    
+population = init_population(100, variables, domains)'''
+    TEST_CODE2 = '''
+list(map(lambda x: fitness_function(courses, rooms, curricula, x), population))'''
+    print(timeit.timeit(TEST_CODE2,SETUP_CODE2, number = 10))
+    
+    variables, domains, constraints, curricula, time_slots = set_up_csp(file_name)
+    courses, rooms, num_days, periods_per_day, unavail_constraints, curricula = read_itc_data_file(file_name)
+    population = init_population(100, variables, domains)
+    
+    fitnessOld = list(map(lambda x: score_solution(file_name, x), population))
+    print(sum(fitnessOld))
+    
+    fitnessNew = list(map(lambda x: fitness_function(courses, rooms, curricula, x), population))
+    print(sum(fitnessNew))
+    
+    print(fitnessOld == fitnessNew)
+    
+    
 def main_func(file_name, output_file=None):
     print ('Performing GA Analysis on '+file_name)
     # Read in the ITC data file and do the pre-process necessary to convert the raw data into
     # variables, domains and constraints
     variables, domains, constraints, curricula, time_slots = set_up_csp(file_name)
-    start = timer()
+    courses, rooms, num_days, periods_per_day, unavail_constraints, curricula = read_itc_data_file(file_name)
+    
     population = init_population(100, variables, domains)
-    result, stats = genetic_algorithm(population, lambda x: score_solution(file_name, x), domains, ngen=100, f_thres=0)
+    
+    start = timer()
+    result, stats = genetic_algorithm(population, lambda x: fitness_function(courses, rooms, curricula, x), domains, ngen=100, f_thres=0)
     end = timer()
     print('GA took {:.1f} seconds'.format(end - start)) # Time in seconds, e.g. 5.38091952400282
-    print(result)
+
     display_solution_in_table(result, time_slots, output_file)
+    
     df = pd.DataFrame(stats)
     fig = plt.figure(1)
     ax = plt.subplot(111)
@@ -257,14 +301,20 @@ def main_func(file_name, output_file=None):
 
 # -------------------------------------------------------------------------------------
 if __name__ == "__main__":
-    if len(sys.argv)==2 and os.path.exists(sys.argv[1]):
-        file_name = sys.argv[1]
-    else:
-        file_name = '../../Data/ITC-2007/comp01.ctt.txt'
-    # file_name = '../../Data/ITC-2007/toy_prob.ctt.txt'
-
+    
     # if you want to generate an output file of the schedule
     # output_file = '/Users/brucks/Desktop/baseline_comp01.txt'
     output_file = None  # if not
+    
+    if len(sys.argv)==2 and os.path.exists(sys.argv[1]):
+        file_name = sys.argv[1]
+        main_func(file_name, output_file)
+    elif (sys.argv)==3 and os.path.exists(sys.argv[1]) and sys.argv[2] == 'test':
+        file_name = sys.argv[1]
+        perf_test(file_name)
+    else:
+        file_name = '../../Data/ITC-2007/comp01.ctt.txt'
+        main_func(file_name, output_file)
 
-    main_func(file_name, output_file)
+    
+
